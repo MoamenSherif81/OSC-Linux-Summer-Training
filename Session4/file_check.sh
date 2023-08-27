c3a52bfff898504c15cd5e900ffdad815e03822f
Moamen Sherif <moamenyt@gmail.com>
2023-08-28 01:53:00 +0300

    Upload session 4 task

---
+++ Session4/file_check.sh
@@ -0,0 +1,11 @@
+filename=$1
+directory=$2
+
+if [ -e "$directory/$filename" ]; then
+    echo "File exists"
+    echo "Contents of $filename:"
+    cat "$directory/$filename"
+else
+    echo "File does not exist"
+fi
+
